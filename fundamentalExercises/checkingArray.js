3c01676621c957067de0e14e4b4ee786ca3c9d10
dosite-dev <dositecyiza@gmail.com>
2025-06-24 12:27:35 +0200

    Checking array according to condition and return boolean value

---
+++ fundamentalExercises/checkingArray.js
@@ -0,0 +1,19 @@
+/*
+Directions:
+Create a function called hasEnoughPlayers() that takes the team array as an argument
+ and returns true or false depending on if the array has at least seven players.
+*/
+
+const team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet",
+             "George Weasley", "Fred Weasley", "Harry Potter"]
+
+const hasEnoughPlayers = (team) =>{
+    if(team.length >= 7){
+        return true
+    }
+    else{
+        return false
+    }
+    
+}
+console.log(hasEnoughPlayers(team))
