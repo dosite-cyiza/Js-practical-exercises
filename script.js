2c942fb64b55b7c3a313d453ea5a1a5993aded64
dosite-cyiza <dositecyiza@gmail.com>
2025-07-15 13:01:55 +0200

    Events: How to attach events to HTML elements,to accessed event that was fired by using parameter in handler function, How to access element that have fired the event using event.target

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const changeAllArticleColors =()=>{
-    const constant = document.querySelectorAll("#products article")
-    console.log(constant);
-    for( let ele of constant){
-        const newClass = ele.classList.add("sale")
-    }
-    const changeColor = document.querySelectorAll("#products .sale")
-        changeColor.forEach((ele) => ele.style.backgroundColor ='ffddd2') 
+const attachBuyEvents =() =>{
+const button = document.querySelectorAll("#products button")
+console.log(button)
 
+button.forEach((btn) =>{
+    btn.addEventListener('click',(e) =>console.log(e.currentTarget)
+)
+})
 }
-
-changeAllArticleColors()
-
+attachBuyEvents()
