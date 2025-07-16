9a4b3769325d5d9379324eb07d69d48a0a5602ac
dosite-cyiza <dositecyiza@gmail.com>
2025-07-16 12:39:53 +0200

    Using the getAttribute() method, get the value of the data-id attribute of the article and print it in the console.

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ const button = document.querySelectorAll("#products button")
 button.forEach((ele) =>
 ele.addEventListener('click',(event) =>{
     console.log(ele.parentElement.classList.toggle("sale"))
+    
+    
 }))
 
+const articles = document.querySelectorAll('#products article')
+console.log(articles)
+articles.forEach((article) => {
+console.log(article.getAttribute("data-id"));
+})
 
  
 
